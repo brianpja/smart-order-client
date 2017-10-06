2b5f325da23c857c10d0b8042daf5dfd459ab86a
Brian James <brianpja@gmail.com>
2017-10-06 15:10:16 -0700

    ui router working

--- app.config.js
+++ app.config.js
@@ -7,11 +7,13 @@
 
   function config($stateProvider, $urlRouterProvider, $locationProvider) {
 
-    $locationProvider.html5Mode(true)
+    // $locationProvider.html5Mode(true)
 
     $stateProvider
       .state({
-        
+        name: 'home',
+        url: '',
+        component: 'home'
       })
   }
 
--- home.js
+++ home.js
@@ -1,11 +1,14 @@
 (function() {
   'use strict';
-
+console.log('loading')
   angular.module('app')
     .component('home', {
       controller,
       template: `
-      <h1>and funny and good at coding</h1>
+      <h1>You are so smart and handsome and funny and good at coding</h1>
+      <div ng-repeat="item in $ctrl.items">
+        <div>{{item.name}}</div>
+      </div>
       `
     })
 
@@ -13,10 +16,17 @@
     function controller($state, $http) {
       const vm = this;
 
+      vm.items = [];
+
       vm.$onInit = function() {
+        vm.getItems();
+      }
+
+      vm.getItems = function() {
         $http.get('http://localhost:8000/items')
           .then((response) => {
             console.log(response.data)
+            vm.items = response.data;
           })
       }
     }
